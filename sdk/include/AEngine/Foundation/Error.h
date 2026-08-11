158a825ec7b94cc3b7ccd2ddb4c5f5bbeaedce5f
maneerat3d <maneerat3d+01@gmail.com>
2026-08-11 18:49:30 +0700

    refactor: separate operation identity from errors

--- sdk/include/AEngine/Foundation/Error.h
+++ sdk/include/AEngine/Foundation/Error.h
@@ -1,18 +1,12 @@
 #pragma once
 
+#include <AEngine/Foundation/Operation.h>
+
 #include <cstdint>
 #include <string>
 #include <utility>
 
 namespace aengine {
-
-struct OperationId {
-    std::uint64_t value = 0;
-
-    [[nodiscard]] constexpr bool IsValid() const noexcept { return value != 0; }
-    [[nodiscard]] static OperationId New() noexcept;
-    friend constexpr bool operator==(OperationId, OperationId) = default;
-};
 
 enum class ErrorCode : std::uint32_t {
     Ok = 0,
